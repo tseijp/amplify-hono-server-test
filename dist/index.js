1773c5f42e6ce9ec5b990a29c20eae1dfe688354
tseijp <_@tsei.jp>
2024-12-07 17:57:33 +0900

    chore: wip

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,8 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_server_1 = require("@hono/node-server");
-const serve_static_1 = require("@hono/node-server/serve-static");
-const hono_1 = require("hono");
-const app = new hono_1.Hono();
-app.use("/static/*", (0, serve_static_1.serveStatic)({ root: "./" }));
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
+const app = new Hono();
+app.use("/static/*", serveStatic({ root: "./" }));
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
@@ -26,33 +15,33 @@ app.get("/get", (c) => {
     });
 });
 // POST Route
-app.post("/post", (c) => __awaiter(void 0, void 0, void 0, function* () {
-    const body = yield c.req.text();
+app.post("/post", async (c) => {
+    const body = await c.req.text();
     return c.text(body, 200, {
         "x-post-header": "post-header-value",
     });
-}));
+});
 // PUT Route
-app.put("/put", (c) => __awaiter(void 0, void 0, void 0, function* () {
-    const body = yield c.req.text();
+app.put("/put", async (c) => {
+    const body = await c.req.text();
     return c.text(body, 200, {
         "x-put-header": "put-header-value",
     });
-}));
+});
 // PATCH Route
-app.patch("/patch", (c) => __awaiter(void 0, void 0, void 0, function* () {
-    const body = yield c.req.text();
+app.patch("/patch", async (c) => {
+    const body = await c.req.text();
     return c.text(body, 200, {
         "x-patch-header": "patch-header-value",
     });
-}));
+});
 // DELETE Route
 app.delete("/delete", (c) => {
     return c.text("", 200, {
         "x-delete-header": "delete-header-value",
     });
 });
-(0, node_server_1.serve)({
+serve({
     fetch: app.fetch,
     port,
 });
